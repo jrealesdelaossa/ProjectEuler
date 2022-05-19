37fe995cc0a5755e7f3a27ba35c663b9deed6583
Julián Reales <jrealesdelaossa@gmail.com>
2022-05-18 20:24:31 -0500

    problema 2 finished

--- JavascriptSolutions/problem2.js
+++ JavascriptSolutions/problem2.js
@@ -6,14 +6,24 @@
  * find the sum of the even-valued terms.
  */
 
-function fibonacci(nTermino) {
-  let fibonacciNumber = [1, 1];
-  for (let i = 2; i < nTermino; i++) {
+/**
+ * The Fibonacci series is obtained up to the nth term defined by the |nTerm| parameter.
+ * @param {*} nTerm
+ * @returns Complete series up to the nth term.
+ */
+function fibonacci(nTerm) {
+  let fibonacciNumber = [0, 1];
+  for (let i = 2; i < nTerm; i++) {
     fibonacciNumber[i] = fibonacciNumber[i - 2] + fibonacciNumber[i - 1];
   }
   return fibonacciNumber;
 }
 
+/**
+ * 
+ * @param {*} arr Array containing the Fibonacci series.
+ * @returns The sum of the even numbers in the array.
+ */
 function sumOfPairs(arr) {
   let arrPairs = [];
   for (let index in arr) {
@@ -23,9 +33,9 @@ function sumOfPairs(arr) {
 }
 
 /**
- * * caso limite el 33
+ * Problem answer.
+ *
+ * The 34th term of the Fibonacci series is the last term less than four million,
+ * therefore it is the parameter that is passed to the function |fibonacci|.
  */
-// console.log(fibonacci(33));
-
-var arreglo = fibonacci(33);
-console.log(sumOfPairs(arreglo));
+console.log(sumOfPairs(fibonacci(34))); // 4613732
